22be6f0888b4657f96cad703819695c7fc479caa
Livio <livioalvarenga@gmail.com>
2021-07-02 12:05:05 -0300

    func para add conteudo em qualquer posição

--- Estrutura de dados/lista_ligada.py
+++ Estrutura de dados/lista_ligada.py
@@ -24,6 +24,28 @@ class Lista_ligada():
         self.__inicio = celula
         self.__quantidade += 1
 
+    def inserir(self, posição: int, conteudo):
+        if posição == 0:
+            self.inserir_no_inicio(conteudo)
+            return
+        celula = Celula(conteudo)
+        esquerda = self.__celula(posição-1)
+        celula.proximo = esquerda.proximo
+        esquerda.proximo = celula
+        self.__quantidade += 1
+
+    def __celula(self, posição: int):
+        self.__validar_posição(posição)
+        atual = self.inicio
+        for i in range(0, posição):
+            atual = atual.proximo
+        return atual
+
+    def __validar_posição(self, posição: int):
+        if 0 <= posição < self.quantidade:
+            return True
+        raise IndexError(f'Posição inválida {posição}')
+
     def imprimir(self):
         atual = self.inicio
         for pos in range(0, self.quantidade):
