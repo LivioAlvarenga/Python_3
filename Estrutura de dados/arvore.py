e8aaf5b0c35ad33d1ad5a7fa9ab586132e5c354f
Livio <livioalvarenga@gmail.com>
2021-07-06 14:33:31 -0300

    def inserir filhos e imprimir níveis

--- Estrutura de dados/arvore.py
+++ Estrutura de dados/arvore.py
@@ -5,6 +5,12 @@ class Lista_de_nodos(Lista_duplamente_ligada):
     def __init__(self):
         super().__init__()
 
+    def imprimir(self, nível):
+        atual = self.inicio
+        for i in range(0, self.quantidade):
+            atual.conteudo.imprimir(nível)
+            atual = atual.proximo
+
 
 class Nodo():
     def __init__(self, conteudo) -> None:
@@ -12,8 +18,16 @@ class Nodo():
         self.pai = None
         self.filhos = None
 
-    def imprimir(self):
-        print(self.conteudo)
+    def imprimir(self, nível=0):
+        print(f'{" "*nível}- {self.conteudo}')
+        if self.filhos:
+            self.filhos.imprimir(nível + 1)
+
+    def inserir_filhos(self, filho):
+        if self.filhos is None:
+            self.filhos = Lista_de_nodos()
+        nodo = Nodo(filho)
+        self.filhos.inserir_no_fim(nodo)
 
 
 class Arvore():
